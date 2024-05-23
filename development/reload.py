a23c2370ffd53d5d3468ac4c8dc852d417a103de
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-05-23 11:17:34 +0200

    Removed print statement

--- development/reload.py
+++ development/reload.py
@@ -7,7 +7,6 @@ def reload(module: types.ModuleType) -> None:
     """
     Recursively reload modules.
     """
-    print(type(module))
     for attribute_name in dir(module):
         attribute = getattr(module, attribute_name)
         if type(attribute) is types.ModuleType:
