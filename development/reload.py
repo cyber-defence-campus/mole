b5badadb8fe4c3724d78ac57803c83c8fd801a13
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-06-13 16:30:19 +0200

    Filter out modules

--- development/reload.py
+++ development/reload.py
@@ -7,6 +7,8 @@ def reload(module: types.ModuleType) -> None:
     """
     Recursively reload modules.
     """
+    if module.__name__ in ("binaryninja", "z3"):
+        return
     for attribute_name in dir(module):
         attribute = getattr(module, attribute_name)
         if type(attribute) is types.ModuleType:
