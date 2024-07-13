88135606c18c1aa12c9e56eef68a2b519e18273f
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-07-13 13:33:27 +0200

    Moved develoment requirements to extras_require

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'mole',
-    version = '0.0.1',
-    packages = find_packages(include = ['mole', 'mole.*']),
+    name = "mole",
+    version = "0.0.1",
+    description = "A Binary Ninja plugin for vulneraiblity discovery",
+    packages = find_packages(include = ["mole", "mole.*"]),
     install_requires = [
-        'debugpy==1.8.1',
-        'termcolor==2.4.0'
+        "termcolor==2.4.0"
     ],
+    extras_require = {
+        "develop": [
+            "debugpy==1.8.1",
+        ]
+    },
     entry_points = {
-        'console_scripts': [
-            'mole=mole.plugin:main'
+        "console_scripts": [
+            "mole=mole.plugin:main"
         ]
     }
 )
