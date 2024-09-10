4425e9216ec0176719d47fb9cc24c02970712c5b
Damian Pfammatter <pfammatter.damian@gmail.com>
2024-09-10 13:11:52 +0200

    Remove script for release notes

--- development/generate-release-notes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Generate release notes based on the latest tag
-LAST_TAG=$(git describe --tags --abbrev=0)
-RELEASE_NOTES=$(git log ${LAST_TAG}..HEAD --oneline)
-
-# Output release notes
-echo "## Release Notes\n\n$RELEASE_NOTES"
