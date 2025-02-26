8058f9a45435ff9365863838314ee184b720ad4d
Damian Pfammatter <pdamian@users.noreply.github.com>
2025-02-26 12:42:32 +0100

    54 tracing ssa variable aliases (#60)
    - Improved handling of `MediumLevelILVarAliased` instructions
    - Added helper function for SsaVariables
    - Improved unit-tests with respect to pointer analysis

--- mole/common/help.py
+++ mole/common/help.py
@@ -59,10 +59,19 @@ class VariableHelper:
 
     @staticmethod
     def get_var_info(
-            var: bn.SSAVariable
+            var: bn.Variable
         ) -> str:
         """
         This method returns a string with information about the variable `var`.
+        """
+        return f"{var.name}"
+    
+    @staticmethod
+    def get_ssavar_info(
+            var: bn.SSAVariable
+        ) -> str:
+        """
+        This method returns a string with information about the SSA variable `var`.
         """
         return f"{var.name}#{var.version}"
 
