6911fe77bc0a26a250f9bd5611acc443e08755d1
Damian Pfammatter <pdamian@users.noreply.github.com>
2025-05-02 15:36:56 +0200

    Testing MLIL_STORE_STRUCT

---
+++ test/src/struct-01.asm
@@ -0,0 +1,20 @@
+; nasm -f elf64 src/struct-01.asm -o bin/struct-01.o
+; ld bin/struct-01.o -o bin/struct-01.linux_x86_64
+; rm bin/struct-01.o
+
+section .data
+    my_struct:                  ; Structure with two integers
+        dd 0                    ; field_a (4 bytes)
+        dd 0                    ; field_b (4 bytes)
+
+section .text
+global _start
+
+_start:
+    lea rdi, [rel my_struct]    ; Load address of my_struct into rdi
+    mov dword [rdi], 42         ; Write 42 to field_a (offset 0)
+    mov dword [rdi+4], 1337     ; Write 1337 to field_b (offset 4)
+
+    mov eax, 60                 ; syscall: exit
+    xor edi, edi                ; status 0
+    syscall                     ; Exit cleanly using Linux syscall
