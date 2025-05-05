66b4ce2f653d59d2da53e099c45a9ad196bf29a2
wizche <sergio.paganoni@gmail.com>
2025-05-05 14:21:55 +0200

    Refactor BackgroundTask and ProgressCallback classes to improve argument handling and add type annotations

--- mole/common/task.py
+++ mole/common/task.py
@@ -1,7 +1,6 @@
 from mole.common.log import log
-from typing import Any, Callable, Optional, Tuple
+from typing import Any, Callable, Optional, Tuple, Protocol, TypeVar, Generic
 import binaryninja as bn
-
 
 tag = "Mole.Task"
 
@@ -29,6 +28,13 @@ class BackgroundTask(bn.BackgroundTaskThread):
         self._results: Any = None
         return
 
+    def set_args(self, *args: Any, **kwargs: Any) -> None:
+        """
+        This method sets the arguments for the background task.
+        """
+        self._args = args
+        self._kwargs = kwargs
+
     def run(self) -> None:
         """
         This method runs the background task.
@@ -45,3 +51,30 @@ class BackgroundTask(bn.BackgroundTaskThread):
         """
         self.join()
         return self._results
+
+
+T = TypeVar("T")
+
+
+class ProgressCallback(Protocol, Generic[T]):
+    def progress(self, msg: str) -> None: ...
+    def cancelled(self) -> bool: ...
+    def new_result(self, result: T) -> None: ...
+
+
+class BackgroundTaskProgress(ProgressCallback, Generic[T]):
+    def __init__(
+        self, task: BackgroundTask, on_result: Optional[Callable[[T], None]] = None
+    ) -> None:
+        self.task = task
+        self.on_result = on_result
+
+    def progress(self, msg: str) -> None:
+        self.task.progress = msg
+
+    def cancelled(self) -> bool:
+        return self.task.cancelled
+
+    def new_result(self, result: T) -> None:
+        if self.on_result:
+            self.on_result(result)
