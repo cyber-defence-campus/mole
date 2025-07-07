bb26f5265c708457d465a4e80c41f77868b18e54
Damian Pfammatter <pdamian@users.noreply.github.com>
2025-07-07 14:34:47 +0200

    Cleanup

--- test/src/struct-01.c
+++ test/src/struct-01.c
@@ -45,28 +45,6 @@ int main(int argc, char *argv[]) {
 
     // Sink
     memcpy(name, p->name, BUF_LEN);
-
     printf("Hello %s!\n", name);
-
-
-    // struct my_struct1 s1 = {1337, 1338};
-    // // struct my_struct1 s2;
-    // // my_struct2 s3 = {1339, 1340};
-    // // my_struct2 s4;
-
-    // // volatile struct my_struct1 *vs1 = &s1;
-    // // volatile struct my_struct1 *vs2 = &s2;
-    // s1.field_a += argc;
-    // s1.field_b += argc;
-
-    // // *vs2 = *vs1;
-    // // s4 = s3;
-    // // memcpy(&s2, &s1, sizeof(struct my_struct1));
-
-    // fprintf(stdout, "s1.field_a = %d\n", s1.field_a);
-    // fprintf(stdout, "s1.field_b = %d\n", s1.field_b);
-    // // fprintf(stdout, "s4.field_a = %d\n", s4.field_a);
-    // // fprintf(stdout, "s4.field_b = %d\n", s4.field_b);
-    
     return EXIT_SUCCESS;
 }
