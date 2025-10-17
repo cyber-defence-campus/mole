80ca6f2e92be2ac3df1054634dd879acfc3f31e3
Damian Pfammatter <pfammatter.damian@gmail.com>
2025-10-17 12:58:43 +0200

    Added unit-test for non-constant pointer dereferencing

--- tests/data/src/load-02.c
+++ tests/data/src/load-02.c
@@ -3,14 +3,20 @@
 
 /*
 Testcase Description:
-- `MediumLevelILLoadSsa` with `MediumLevelILVarSsa` as source
+- Load using non-constant pointer dereferencing
 */
 
-__attribute__ ((noinline, optimize("O0")))
+#define BUF_SIZE 32
+
 int main(int argc, char *argv[]) {
-    if(argc >= 2) {
-        argv[1] = getenv("CMD");
-        system(argv[1]);
+    if(argc >= 3) {
+        char path[BUF_SIZE];
+        char cmd[BUF_SIZE];
+        argv[1] = getenv("FILE_PATH");
+        argv[2] = getenv("SEARCH_TERM");
+        snprintf(path, sizeof(path), "%s", argv[1]);
+        snprintf(cmd, sizeof(cmd), "grep %s %s", argv[2], path);
+        system(cmd);
     }
     return 0;
 }
