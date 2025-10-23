9617129d1c80491e79df81b437baaa45ed47751c
Damian Pfammatter <pfammatter.damian@gmail.com>
2025-10-23 15:55:28 +0200

    Added unit-tests for pointer dereferncing

---
+++ tests/slicing/test_pointer_deref.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from tests.slicing import TestSlicing
+from typing import List
+
+
+class TestPointerDeref(TestSlicing):
+    def test_load_01(self, filenames: List[str] = ["load-01"]) -> None:
+        self.assert_paths(
+            src=[("getenv", None)],
+            snk=[("system", 1)],
+            call_chains=[["main"]],
+            filenames=filenames,
+        )
+        return
+
+    def test_load_02(self, filenames: List[str] = ["load-02"]) -> None:
+        self.test_load_01(filenames=filenames)
+        return
+
+    def test_load_03(self, filenames: List[str] = ["load-03"]) -> None:
+        self.assert_paths(
+            src=[("getenv", None)],
+            snk=[("system", 1)],
+            call_chains=[["main"], ["main"]],
+            filenames=filenames,
+        )
+        return
+
+    def test_load_04(self, filenames: List[str] = ["load-04"]) -> None:
+        self.test_load_03(filenames=filenames)
+        return
+
+    def test_load_05(self, filenames: List[str] = ["load-05"]) -> None:
+        self.assert_paths(
+            src=[("getenv", None)],
+            snk=[("memcpy", 2), ("memcpy", 3)],
+            call_chains=[["main"], ["main"]],
+            filenames=filenames,
+        )
+        return
