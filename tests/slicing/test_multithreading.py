1cae3f559293814d7ce148a6590a496d0d5c933c
Damian Pfammatter <pfammatter.damian@gmail.com>
2025-10-24 08:48:37 +0200

    Split multithreading unit-tests

--- tests/slicing/test_multithreading.py
+++ tests/slicing/test_multithreading.py
@@ -6,15 +6,7 @@ import binaryninja as bn
 
 class TestMultiThreading(TestSlicing):
     def test_consistency_01(
-        self,
-        filenames: List[str] = [
-            "function_calling-02",
-            "name_mangling-01",
-            "load-05",
-            "pointer_analysis-06",
-            "simple_http_server-03",
-            "memcpy-05",
-        ],
+        self, filenames: List[str] = ["function_calling-02"]
     ) -> None:
         for file in self.load_files(filenames):
             # Load and analyze test binary with Binary Ninja
@@ -36,4 +28,28 @@ class TestMultiThreading(TestSlicing):
                 )
             # Close binary
             bv.file.close()
+        return
+
+    def test_consistency_02(self, filenames: List[str] = ["name_mangling-01"]) -> None:
+        self.test_consistency_01(filenames)
+        return
+
+    def test_consistency_03(self, filenames: List[str] = ["load-05"]) -> None:
+        self.test_consistency_01(filenames)
+        return
+
+    def test_consistency_04(
+        self, filenames: List[str] = ["pointer_analysis-06"]
+    ) -> None:
+        self.test_consistency_01(filenames)
+        return
+
+    def test_consistency_05(
+        self, filenames: List[str] = ["simple_http_server-03"]
+    ) -> None:
+        self.test_consistency_01(filenames)
+        return
+
+    def test_consistency_06(self, filenames: List[str] = ["memcpy-05"]) -> None:
+        self.test_consistency_01(filenames)
         return
