ffe037a7249efa87bd9b7a3d61237806e9da1b0d
Damian Pfammatter <pfammatter.damian@gmail.com>
2025-10-30 11:37:34 +0100

    Added test-case

---
+++ tests/data/src/function_out_parameter-01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Testcase Description:
+- Function with output parameter
+*/
+
+int get_cmd(char **out_cmd){
+    char *env_cmd = getenv("CMD");
+    if (env_cmd != 0) {
+        *out_cmd = env_cmd;
+        return 0;
+    }
+    return -1;
+}
+
+int main() {
+    char *cmd = NULL;
+    if (get_cmd(&cmd) == 0) {
+        system(cmd);
+    } else {
+        fprintf(stderr, "CMD environment variable not set.\n");
+    }
+    return 0;
+}
