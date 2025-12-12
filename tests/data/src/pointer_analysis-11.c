49f58da50286b4a51717e1e63801d023b6901f4f
Damian Pfammatter <pfammatter.damian@gmail.com>
2025-12-12 15:09:54 +0100

    Cleanup

--- tests/data/src/pointer_analysis-11.c
+++ tests/data/src/pointer_analysis-11.c
@@ -4,20 +4,20 @@
 #define CMD_LEN 64
 
 __attribute__ ((noinline, optimize("O0")))
-char* validate(char *cmd) {
+char* validate(char* cmd) {
     if(cmd == NULL) cmd = "";
     return cmd;
 }
 
 __attribute__ ((noinline))
-int execute(char *cmd) {
+int execute(char* cmd) {
     cmd = validate(cmd);
     return system(cmd);
 }
 
 int main(int argc, char *argv[]) {
     char cmd[CMD_LEN];
-    char *env_cmd = getenv("CMD");
-    snprintf(cmd, CMD_LEN, "%s", env_cmd);
+    char* env = getenv("CMD");
+    snprintf(cmd, CMD_LEN, "%s", env);
     return execute(cmd);
 }
